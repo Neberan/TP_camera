c1a9a8dfbf26e6e34263b44aaac31f097f6a38e2
Neberan <johann77166@hotmail.fr>
2017-10-17 14:36:33 +0200

    Mise en place du bouton

--- Camera/app/src/main/java/fr/upec/moulart/camera/MainActivity.java
+++ Camera/app/src/main/java/fr/upec/moulart/camera/MainActivity.java
@@ -1,8 +1,10 @@
 package fr.upec.moulart.camera;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private Camera mCamera = null;
     private CameraView mCameraView = null;
     private static final int REQUEST_CALL = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,18 +56,16 @@ public class MainActivity extends AppCompatActivity {
 
 }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
-            case REQUEST_CALL:
-            {
-                if (grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
-                    //startActivity(intent);
-                }else{
-                    ////
-                }
+    public void onBtnClicked(View v) {
+
+        if (v.getId() == R.id.button){
+
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
+
     }
 
 }
