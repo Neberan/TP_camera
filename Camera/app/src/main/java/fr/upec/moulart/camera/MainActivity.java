ff0c59602497f854029ebd96fd34a80a3773af40
Neberan <johann77166@hotmail.fr>
2017-10-17 10:55:22 +0200

    Verif camera
    Vérification camera

--- Camera/app/src/main/java/fr/upec/moulart/camera/MainActivity.java
+++ Camera/app/src/main/java/fr/upec/moulart/camera/MainActivity.java
@@ -3,21 +3,25 @@ package fr.upec.moulart.camera;
 import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView texte;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        texte = (TextView) findViewById(R.id.textView);
     }
 
     private boolean hasCamera() {
         if (getPackageManager().hasSystemFeature(
                 PackageManager.FEATURE_CAMERA_FRONT)){
-            return true;
+            texte.setText("oui");
         } else {
-            return false;
+            texte.setText("non");
         }
     }
 }
